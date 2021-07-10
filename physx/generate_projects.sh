b566e491f3799c9a9912c1f59de605a13e270458
Admer <49600504+Admer456@users.noreply.github.com>
2021-07-10 21:14:44 +0200

    Update project files

--- physx/generate_projects.sh
+++ physx/generate_projects.sh
@@ -3,11 +3,7 @@
 export PHYSX_ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 export PM_PxShared_PATH="$PHYSX_ROOT_DIR/../pxshared"
 export PM_CMakeModules_PATH="$PHYSX_ROOT_DIR/../externals/cmakemodules"
-export PM_opengllinux_PATH="$PHYSX_ROOT_DIR/../externals/opengl-linux"
-export PM_TARGA_PATH="$PHYSX_ROOT_DIR/../externals/targa"
-export PM_CGLINUX_PATH="$PHYSX_ROOT_DIR/../externals/cg-linux"
-export PM_GLEWLINUX_PATH="$PHYSX_ROOT_DIR/../externals/glew-linux"
-export PM_PATHS="$PM_opengllinux_PATH;$PM_TARGA_PATH;$PM_CGLINUX_PATH;$PM_GLEWLINUX_PATH"
+export PM_PATHS=""
 
 
 cd "$( dirname "${BASH_SOURCE[0]}" )"
